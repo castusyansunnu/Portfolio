3826092707f41f392c73d03a2e4473f0c48ea7c1
Castus YANSUNNU <castusyansunnu90@gmail.com>
2025-11-27 17:32:31 +0100

    nouvelle mise a jour

--- index.php
+++ index.php
@@ -4,11 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="styleshett" href="index.css">
 </head>
 <body>
+    <!--HEADER -->
+    <herder class="herder">
+        <div class="contener">
+            <h1>Mon portfolio</h1>
+            <nav>
+                <a href="">Acceuil</a>
+                <a href="">Apropos</a>
+                <a href="">Projet</a>
+            
+            </nav>
+        </div>
+    </herder>
+
+    <!--HERO -->
+     <Section class ="Section">
+        <div class="hero-contener">
+            <p class= "titre-modifier"> YANSUNNU Castus</p>
+
+            <h1>
+                ETUDIANT EN SYSTEME INFORMATIQUE ET LOGICIEL
+                <span class="texte-coloré">DEVELOPPER FULL-STARCK</span>
+            </h1>
+            <p classe="description">
+               Étudiant en troisième année d'informatique, passionné par la création de
+               solutions logicielles innovantes et la résolution de défis techniques complexes.
+            </p>
+            
+            <div class="hero-bouton">
+                <a href="">Mes projet</a>
+                <a href="">Contact</a>
+            </div>
+
+    <div  class="hero-icone">
+        <a href="#" target="_blank" aria-label="Lien vers GitHub"><i class="fab fa-github"></i></a>
+        <a href="#" target="_blank" aria-label="Lien vers LinkedIn"><i class="fab fa-linkedin"></i></a>
+        <a href="mailto:votre.email@domaine.com" aria-label="Envoyer un email"><i class="fas fa-envelope"></i></a>
+    </div>
+
+        </div>
+
+     </Section>
+
+        
     
 </body>
 </html>
-
-
-
